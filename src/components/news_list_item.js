4e9629184d25b0b0cf28bb2a7c1c16d7d8966db8
Andy Serrato <andres.serrato@thinkwrap.com>
2018-12-27 22:15:31 +0100

    npm eject and the use of css modules

--- src/components/news_list_item.js
+++ src/components/news_list_item.js
@@ -1,32 +1,10 @@
 import React from 'react'
-//import {css} from 'glamor'
-import classes from '../css/styles.css'
+import styles from '../css/NewsListItem.module.css'
 
 const NewsItem = ({item}) => {
 
- /**
-  * GLAMOR
-  */
- /* let news_item = css({
-    padding: '20px',
-    boxSizing: 'border-box',
-    borderBottom: '1px solid grey',
-    '_hover': {
-      color:'red'
-    },
-    '@media(max-width: 500px)': {
-      color: 'blue'
-    }
-  })
-
-  let item_grey = css({
-    background: 'lightgrey'
-  })
-  <div className={`${news_item} ${item_grey}`}>
-  */
-
   return (
-    <div className={classes.news_item}>
+    <div className={styles.news_item}>
       <h3>{item.title}</h3>
       <div>
         {item.feed}
