3b1636d6c7d178cd18af83348bc2e3f15fde84ac
Andy Serrato <andres.serrato@thinkwrap.com>
2018-12-27 23:00:56 +0100

    Updates

---
+++ precache-manifest.bcd66e18cfc722008ee7e2ea07948f49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "247649ee041eb0cc2d0e",
+    "url": "/01_react_basics/static/js/runtime~main.247649ee.js"
+  },
+  {
+    "revision": "294d33a83788fcd63625",
+    "url": "/01_react_basics/static/js/main.294d33a8.chunk.js"
+  },
+  {
+    "revision": "12d00c312a8d914423d8",
+    "url": "/01_react_basics/static/js/1.12d00c31.chunk.js"
+  },
+  {
+    "revision": "294d33a83788fcd63625",
+    "url": "/01_react_basics/static/css/main.c7dc8e7d.chunk.css"
+  },
+  {
+    "revision": "2726baa376d07bca1ff4720f9cf37c82",
+    "url": "/01_react_basics/index.html"
+  }
+];
